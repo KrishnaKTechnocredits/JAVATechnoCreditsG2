30bb6136648f10359fd59384c0fb33c7240a288f
Krishna_Kanani <krishna.kanani@hp.com>
2019-01-20 10:09:15 +0530

    Project upload

---
+++ JavaAssignment/src/atul/TestCheck.java
@@ -0,0 +1,7 @@
+package atul;
+public class TestCheck {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Git Demo");
+	}
+}
---
+++ JavaAssignment/src/technoCredits/TestCheck.java
@@ -0,0 +1,7 @@
+package technoCredits;
+public class TestCheck {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Git Demo");
+	}
+}
