475e3af7f062e394cc50ad4fd1db69b1cbb05c1b
RahulMTechnocredits <mishra260rahul@gmail.com>
2019-01-22 19:04:36 +0530

    Assignment_Q2_RahulM

---
+++ JavaAssignment/src/rahulM/Q2.java
@@ -0,0 +1,14 @@
+//WAP to print details of student As roll number,name and marks taking input from the user
+package rahulM;
+class Q2
+{
+	public static void main (String [] a)
+	{
+		int rNo= Integer.parseInt(a[0]);
+		String name= a[1];
+		int marks= Integer.parseInt(a[2]);
+		System.out.println("The roll number is: "+rNo+"\nName of the student: "+name+"\nMarks obtained: "+marks);
+		
+	}
+	
+}
