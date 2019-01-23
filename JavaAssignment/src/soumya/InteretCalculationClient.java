cb51f574fa18cde826be594f75e7630dce69049e
SoumyaSTechnocredits <soumyasharma28@gmail.com>
2019-01-23 23:35:15 +0530

    Assignment_Q5_Soumya

---
+++ JavaAssignment/src/soumya/InteretCalculationClient.java
@@ -0,0 +1,11 @@
+/* 
+ * This Program is used to calculate Simple interest
+ */
+package soumya;
+
+public class InteretCalculationClient {
+	public static void main(String[] args) {
+		InterestCalculation_Service o1 = new InterestCalculation_Service();
+		o1.interestCalculator();
+	}
+}
