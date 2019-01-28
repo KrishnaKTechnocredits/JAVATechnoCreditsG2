a75313c45d0ab8ad7d21c3a38a5b08d377d10b88
Monali <mona311988@gmail.com>
2019-01-29 00:09:24 +0530

    Seperate Methods created-Monali

--- JavaAssignment/src/monali/Q3_SquareCube.java
+++ JavaAssignment/src/monali/Q3_SquareCube.java
@@ -3,9 +3,18 @@ package monali;
 public class Q3_SquareCube {
 	public static void main(String[] args) {
 		int i = Integer.parseInt(args[0]);
-		int square = i * i;
-		int cube = i * i * i;
-		System.out.println("Square of "+i+ " is: "+ square);
-		System.out.println("Cube of "+i+" is: " + cube);
+		Square(i);
+		Cube(i);
+	}
+		static void Square(int num)
+		{
+			int square = num * num;
+			System.out.println("Square of "+num+ " is: "+ square);
+		}
+		
+		static void Cube(int num)
+		{
+		int cube = num * num * num;
+		System.out.println("Cube of "+num+" is: " + cube);
 	}
 }
