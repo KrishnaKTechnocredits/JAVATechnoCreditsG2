49fa548d02654eb3b47bc0e9cb082ec2510db3f9
SoumyaSTechnocredits <soumyasharma28@gmail.com>
2019-02-08 01:04:21 +0530

    Assignment3_Class_Object_Q1_Q2_Q3_Q4_Q5_Q6_Q7

---
+++ JavaAssignment/src/soumya/BookTest.java
@@ -0,0 +1,11 @@
+/* 
+ * This Program will display book details
+ */
+package soumya;
+
+public class BookTest {
+	public static void main(String[] args) {
+		Book details = new Book("War and Peace", "The Russian Messenger", "Leo Tolstoy");
+		details.display();
+	}
+}
---
+++ JavaAssignment/src/soumya/TestEmployee.java
@@ -0,0 +1,11 @@
+/* 
+ * This Program will display employee details
+ */
+package soumya;
+
+public class TestEmployee {
+	public static void main(String[] args) {
+		Employee details = new Employee(101, "Soumya", 20000);
+		details.displayDetails();
+	}
+}
