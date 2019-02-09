80bd9e02bb0c82015cd46dce66696ca28e5bfc97
Monali <mona311988@gmail.com>
2019-02-09 19:19:21 +0530

    Changes commited

--- JavaAssignment/src/monali/Book.java
+++ JavaAssignment/src/monali/Book.java
@@ -4,15 +4,16 @@ public class Book {
 	String title;
 	String bookPublisher;
 	String author;
-	Book(String t1,String bpublisher,String authr )
-	{
-		this.title=t1;
-		this.bookPublisher=bpublisher;
-		this.author=authr;
+
+	Book(String t1, String bpublisher, String authr) {
+		this.title = t1;
+		this.bookPublisher = bpublisher;
+		this.author = authr;
 	}
-	void displayBookDetails()
-	{
-		System.out.println("Details of book are: "+"Title of book is "+title+"  Book Publisher: "+bookPublisher+" Author:  "+author);
+
+	void displayBookDetails() {
+		System.out.println("Details of book are: " + "Title of book is " + title + "  Book Publisher: " + bookPublisher
+				+ " Author:  " + author);
 	}
 
 }
